461df752627ed560695173800e7baa227662897c
Ruben Cancino <rubencancino6@gmail.com>
2015-02-28 13:39:09 -0600

    Importar balanza

--- grails-app/domain/com/luxsoft/sw4/econta/Balanza.groovy
+++ grails-app/domain/com/luxsoft/sw4/econta/Balanza.groovy
@@ -20,7 +20,8 @@ class Balanza {
 
     static constraints = {
     	versionSat inList:['1.0']
-    	rfc matches:"[A-ZÑ&]{3,4}[0-9]{2}[0-1][0-9][0-3][0-9][A-Z0-9]?[A-Z0-9]?[0-9A-Z]?"
+    	//rfc matches:"[A-ZÑ&]{3,4}[0-9]{2}[0-1][0-9][0-3][0-9][A-Z0-9]?[A-Z0-9]?[0-9A-Z]?"
+    	rfc size:12..13
     	mes inList:['01','02','03','04','05','06','07','08','09','10','11','12']
     	ejercicio range:2014..2099
     	acuse nullable:true,maxSize:(1024 * 512)  // 50kb para almacenar el acuse
--- grails-app/services/com/luxsoft/sw4/econta/ImportadorDeBalanzaService.groovy
+++ grails-app/services/com/luxsoft/sw4/econta/ImportadorDeBalanzaService.groovy
@@ -9,21 +9,23 @@ import org.apache.commons.lang.StringUtils
 @Transactional(readOnly=false)
 class ImportadorDeBalanzaService {
 
-	def dataSource_impap
+    def dataSource_impap
+
+    def grailsApplication
 
     @NotTransactional
     def importar(Empresa empresa,Integer ejercicio,Integer mes) {
-    	
-    	switch(empresa.clave) {
-    		case 'IMPAP':
-    			importarImpap(empresa,ejercicio,mes)
-    			break
-            case 'PAPEL'
+        
+        switch(empresa.clave) {
+            case 'IMPAP':
+                importarImpap(empresa,ejercicio,mes)
+                break
+            case 'PAPEL':
                 importarBalanzaDePapel(empresa,ejercicio,mes)
                 break
-    		default:
-    			break
-    	}
+            default:
+                break
+        }
 
     }
 
@@ -31,58 +33,90 @@ class ImportadorDeBalanzaService {
     def importarImpap(Empresa empresa,Integer ejercicio,Integer mes){
 
         log.info "IMPORTANDO balanza de $empresa $ejercicio -  $mes"
-    	
-    	def found=Balanza.findByEmpresaAndEjercicioAndMes(empresa,ejercicio,mes)
-    	if(!found){
-    		found=new Balanza(empresa:empresa
-    			,ejercicio:ejercicio
-    			,mes:StringUtils.leftPad(mes.toString(), 2, '0')
-    			,rfc:empresa.rfc)
-    	}
-    	if(found.acuse){
-    		throw new BalanzaException(message:'Balanza ya cerrada no es modificable')
-    	}
+        
+        def found=Balanza.findByEmpresaAndEjercicioAndMes(empresa,ejercicio,mes)
+        if(!found){
+            found=new Balanza(empresa:empresa
+                ,ejercicio:ejercicio
+                ,mes:StringUtils.leftPad(mes.toString(), 2, '0')
+                ,rfc:empresa.rfc)
+        }
+        if(found.acuse){
+            throw new BalanzaException(message:'Balanza ya cerrada no es modificable')
+        }
       
         def db=new Sql(dataSource_impap)
         println impapSql
-    	def res=db.eachRow(impapSql,[ejercicio,mes,ejercicio,mes]) { row ->
+        def res=db.eachRow(impapSql,[ejercicio,mes,ejercicio,mes]) { row ->
             println 'Procesando: '+row
-    		def cuenta=Cuenta.findByEmpresaAndClave(empresa,row.clave)
-    		
-    		if(cuenta){
-    			found.addToPartidas(cuenta:cuenta
-    				,saldoIni:row.saldo_inicial
-    				,debe:row.debe
-    				,haber:row.haber
-    				,saldoFin:row.saldo_final
-    				)
-    			log.info "Cuenta registrada: $cuenta.clave"
-    		}else{
-    			log.info "Cuenta no registrada $cuenta"
-    		}
-    		//if(!cuenta) throw new BalanzaException(message:"No existe la cuenta: "+row.clave+ " para la empresa: $empresa")
+            def cuenta=Cuenta.findByEmpresaAndClave(empresa,row.clave)
+            
+            if(cuenta){
+                found.addToPartidas(cuenta:cuenta
+                    ,saldoIni:row.saldo_inicial
+                    ,debe:row.debe
+                    ,haber:row.haber
+                    ,saldoFin:row.saldo_final
+                    )
+                log.info "Cuenta registrada: $cuenta.clave"
+            }else{
+                log.info "Cuenta no registrada $cuenta"
+            }
+            //if(!cuenta) throw new BalanzaException(message:"No existe la cuenta: "+row.clave+ " para la empresa: $empresa")
 
-    	}
+        }
         
-	    found.save failOnError:true
-	    return found
+        found.save failOnError:true
+        return found
     }
 
 
     def impapSql=""" 
-    	SELECT (SELECT rfc FROM empresa ) as rfc				
-		,(SELECT COUNT(*) FROM saldo_por_cuenta_contable X JOIN cuenta_contable Y ON(X.cuenta_id=Y.ID) WHERE Y.padre_id IS NULL AND X.YEAR=? AND X.MES=? AND X.CUENTA_ID NOT IN(29,33,254,40,41,42,43))  AS CTAS				
-		,S.YEAR,S.MES,S.CUENTA_ID ,C.CLAVE,C.DESCRIPCION,S.saldo_inicial,S.DEBE,S.HABER,S.saldo_final  				
-		FROM saldo_por_cuenta_contable S JOIN cuenta_contable C ON(S.cuenta_id=C.ID) 				
-		WHERE YEAR=? AND MES=? AND S.CUENTA_ID NOT IN(29,33,168,167,254,255,40,228,229,230,231,232,233,234,41,235,236,237,238,239,240,241,42,242,243,244,43,245,246,247,308)				
+        SELECT (SELECT rfc FROM empresa ) as rfc                
+        ,(SELECT COUNT(*) FROM saldo_por_cuenta_contable X JOIN cuenta_contable Y ON(X.cuenta_id=Y.ID) WHERE Y.padre_id IS NULL AND X.YEAR=? AND X.MES=? AND X.CUENTA_ID NOT IN(29,33,254,40,41,42,43))  AS CTAS                
+        ,S.YEAR,S.MES,S.CUENTA_ID ,C.CLAVE,C.DESCRIPCION,S.saldo_inicial,S.DEBE,S.HABER,S.saldo_final               
+        FROM saldo_por_cuenta_contable S JOIN cuenta_contable C ON(S.cuenta_id=C.ID)                
+        WHERE YEAR=? AND MES=? AND S.CUENTA_ID NOT IN(29,33,168,167,254,255,40,228,229,230,231,232,233,234,41,235,236,237,238,239,240,241,42,242,243,244,43,245,246,247,308)                
     """
 
     def importarBalanzaDePapel(Empresa empresa,Integer ejercicio,Integer mes){
+        log.info "IMPORTANDO balanza de $empresa $ejercicio -  $mes"
+        
+        def found=Balanza.findByEmpresaAndEjercicioAndMes(empresa,ejercicio,mes)
+        if(!found){
+            found=new Balanza(empresa:empresa
+                ,ejercicio:ejercicio
+                ,mes:StringUtils.leftPad(mes.toString(), 2, '0')
+                ,rfc:empresa.rfc)
+        }
+
+        if(found.acuse){
+            throw new BalanzaException(message:'Balanza ya cerrada no es modificable')
+        }
+
         def file=grailsApplication.mainContext.getResource("/WEB-INF/data/BalanzaDeComprobacionPapelSAEne15.csv").file
+        file.eachLine{line,row ->
+            if(row>1){
+                println line
+                def fields=line.split(";")
+                def cuenta=Cuenta.findByEmpresaAndClave(empresa,fields[0])
+                if(cuenta){
+                    found.addToPartidas(cuenta:cuenta
+                        ,saldoIni:fields[1]
+                        ,debe:fields[2]
+                        ,haber:fields[3]
+                        ,saldoFin:fields[4]
+                        )
+                    log.info "Cuenta registrada: $cuenta.clave"
+                }
+            }
+        }
+        found.save failOnError:true
+        return found
     }
 }
 
 class BalanzaException extends RuntimeException{
-	String message
-	Exception e
+    String message
+    Exception e
 }
